3985ea2f1bafd84a62f060b2f1c40dea6c5e904b
cagdaseksi <cagdas.eksi@hotmail.com>
2018-12-29 10:37:20 +0300

    initial commit

--- AlerViewController/ViewController.swift
+++ AlerViewController/ViewController.swift
@@ -9,12 +9,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var lblMessage: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        createAlert(title: "Do You Like Swift?", message: "Do you?")
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func createAlert (title:String, message:String)
+    {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
+        
+        //CREATING ON BUTTON
+        alert.addAction(UIAlertAction(title: "Yes", style: UIAlertAction.Style.default, handler: { (action) in
+            alert.dismiss(animated: true, completion: nil)
+            self.lblMessage.text = "Yes. I like swift.😍"
+        }))
+        
+        alert.addAction(UIAlertAction(title: "No", style: UIAlertAction.Style.default, handler: { (action) in
+            alert.dismiss(animated: true, completion: nil)
+            self.lblMessage.text = "No. I don't like swift.🤔"
+        }))
+        
+        self.present(alert, animated: true, completion: nil)
+    }
+    
 }
-
